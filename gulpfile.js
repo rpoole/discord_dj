8795e6520a793d4827f4955e99430fe41e5f2b65
Robert Poole <rob44@vt.edu>
2016-02-23 19:01:39 -0500

    more setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,26 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const runSequence = require('run-sequence');
+const shell = require('gulp-shell');
 
-gulp.task('default', () => {
-  gulp.src('discord_dj/src/**/*.js')
+
+gulp.task('default', callback => {
+  runSequence(
+      'transpile',
+      'load',
+      callback);
+});
+
+gulp.task('load', () => {
+  return gulp
+    .src('')
+    .pipe(shell([
+          'node discord_dj/out/bot.js',
+    ]));
+});
+
+gulp.task('transpile', () => {
+  return gulp.src('discord_dj/src/**/*.js')
     .pipe(babel({
       presets: ['es2015']
     }))
